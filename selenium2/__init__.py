87cbbf38c502071a1e1cd663e06276f7e0d62550
长安 <69743019+Moon-du@users.noreply.github.com>
2026-06-24 13:38:09 +0800

    Delete selenium2 directory

--- selenium2/__init__.py
+++
@@ -1,6 +0,0 @@
-# _*_ coding: UTF-8 _*_
-# @Time : 2020/10/26 14:08
-# @Author : moon
-# @Site : www.ysbzc.com
-# @File : page_test.py
-# @Software : PyCharm
--- selenium2/prac.py
+++
@@ -1,8 +0,0 @@
-# _*_ coding: UTF-8 _*_
-# @Time : 2020/10/26 14:08
-# @Author : moon
-# @Site : www.ysbzc.com
-# @File : page_test.py
-# @Software : PyCharm
-
-# 在excel文件里，以百度首页为例，练习8种定位方式，手写xpath表达其中的各个元素
